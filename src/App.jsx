32de572175eaba6db06ed15875f5b609c2265d47
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-10-12 17:05:10 +0530

    developer branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import './App.css'
+import './App.css';
 
-function App() {
+const App = () => {
   
-  return (
-    <>
-      <h1>hell</h1>
-    </>
-  )
-}
 
-export default App
+  return (
+    <div >
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+          <TooltipComponent
+              content="Settings"
+              position="Top"
+            >
+              <button
+                type="button"
+                // onClick={() => setThemeSettings(true)}
+                style={{ background: 'blue', borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+
+            </TooltipComponent>
+          </div>
+         </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
